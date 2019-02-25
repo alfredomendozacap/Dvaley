8bfd91a010bd319fb06c36887bec5379188b16da
Alfredo Mendoza <kick-1930@hotmail.com>
2019-02-24 21:29:28 -0500

    Se hizo el menu responsive mejoras en js y clases del navbar

--- assets/js/scrollbar.js
+++ assets/js/scrollbar.js
@@ -3,13 +3,18 @@
     let navbar = d.getElementById('navbar')
     w.addEventListener('scroll', () => {
         if (pageYOffset > 300) {
-            navbar.style.background = 'rgba(255,255,255,.5)'
-            navbar.style.paddingBottom = '0'
-            navbar.style.fontWeight = '500'
+            navbar.classList.add('scrollY')
         } else {
-            navbar.style.background = 'none'
-            navbar.style.paddingBottom = '1rem'
-            navbar.style.fontWeight = '600'
+            navbar.classList.remove('scrollY')
         }
     })
+    const showMenu = (toggle,menu) => {
+        const navbarToggle = d.getElementById(toggle)
+        const mainMenu = d.getElementById(menu)
+        navbarToggle.addEventListener('click', () => {
+            mainMenu.classList.toggle('show')
+            navbar.classList.toggle('back_theme')
+        })
+    }
+    showMenu('navbar__toggle','main-navbar')
 })(document,console.log,window)

f1b6466074c5ae25675a66e42eb8a5f022bc1bf7
Alfredo Mendoza <kick-1930@hotmail.com>
2019-02-24 22:53:49 -0500

    Se arreglo los conflictos en los estilos y el JS

--- assets/js/scrollbar.js
+++ assets/js/scrollbar.js
@@ -1,20 +1,28 @@
 ;
 ((d,c,w)=>{
-    let navbar = d.getElementById('navbar')
-    w.addEventListener('scroll', () => {
-        if (pageYOffset > 300) {
-            navbar.classList.add('scrollY')
-        } else {
-            navbar.classList.remove('scrollY')
-        }
-    })
-    const showMenu = (toggle,menu) => {
+    const showMenu = (toggle, menu) => {
         const navbarToggle = d.getElementById(toggle)
         const mainMenu = d.getElementById(menu)
+        let navbar = d.getElementById('navbar')
+        let logo = d.getElementById('logo')
         navbarToggle.addEventListener('click', () => {
             mainMenu.classList.toggle('show')
             navbar.classList.toggle('back_theme')
         })
+        
+        w.addEventListener('scroll', () => {
+            if (pageYOffset > 300) {
+                navbar.classList.add('scrollY')
+                logo.classList.add('logo_width')
+                logo.style.transition = 'all ease .5s'
+                mainMenu.classList.add('scrollY-top')
+            } else {
+                navbar.classList.remove('scrollY')
+                logo.classList.remove('logo_width')
+                mainMenu.classList.remove('scrollY-top')
+            }
+        })
+        
     }
     showMenu('navbar__toggle','main-navbar')
 })(document,console.log,window)
